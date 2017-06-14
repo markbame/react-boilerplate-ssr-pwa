09ebdbac858186eeb11fb7a5b829a675be9371e6
Mark Bame <markbame.martires@gmail.com>
2017-06-14 23:28:03 +0800

    initial files v2

---
+++ shared/components/DemoApp/AsyncHomeRoute/HomeRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import config from '../../../../config';
+
+function HomeRoute() {
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>    
+    </div>
+  );
+}
+
+export default HomeRoute;
